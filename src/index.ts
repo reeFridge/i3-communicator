e122e4f60c590f6b112c17b43868ac3ebf9940b5
reeFridge <reefridgerator@gmail.com>
2017-04-13 02:41:28 +0700

    Update using example

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,35 @@
-import net = require('net');
-import childProcess = require('child_process');
+import {Socket} from 'net';
+import {execSync} from 'child_process';
 import console = require('console');
 import path = require('path');
+import Communicator from './communicator';
+import {I3Message, I3MessageType, MAGIC} from './i3-message';
+import {I3Reply} from './i3-reply';
+import I3MessageBuilder from './i3-message-builder';
 
-enum MessageType {
-    COMMAND,
-    GET_WORKSPACES,
-    SUBSCRIBE,
-    GET_OUTPUTS,
-    GET_TREE,
-    GET_MARKS,
-    GET_BAR_CONFIG
-}
+const comm = new Communicator<I3Message>();
 
-enum EventType {
-    WORKSPACE,
-    OUTPUT
-}
-
-const MAGIC_STRING = 'i3-ipc';
-
-interface IIPC {
-    connect(socketPath: string): Promise<net.Socket>;
-    send(type: MessageType, payload: string): void;
-    packMessage(message: Ii3Message): Buffer;
-    unpackMessage(messageBuff: Buffer): Ii3Message;
-}
-
-interface Ii3Message {
-    length: number;
-    type: MessageType;
-    payload: string;
-}
-
-class IPC implements IIPC {
-    private socket: net.Socket;
-
-    connect(socketPath: string): Promise<net.Socket> {
-        return new Promise((resolve, reject) => {
-           this.socket = net.createConnection(socketPath);
-           this.socket.on('connect', () => resolve(this.socket));
-           this.socket.on('error', (err) => reject(err));
-        });
-    }
-
-    send(type: MessageType, payload: string): void {
-        this.socket.write(this.packMessage({
-            length: Buffer.byteLength(payload),
-            type: type,
-            payload: payload
-        }));
-    }
-
-    packMessage(message: Ii3Message): Buffer {
-        const length = Buffer.byteLength(MAGIC_STRING) + 8 + message.length;
-        const buff = new Buffer(length);
-        let offset: number;
-
-        offset = buff.write(MAGIC_STRING);
-        offset = buff.writeInt32LE(message.length, offset);
-        offset = buff.writeInt32LE(message.type as number, offset);
-        buff.write(message.payload, offset);
-
-        return buff;
-    }
-
-    unpackMessage(messageBuff: Buffer): Ii3Message {
-        const length = messageBuff.readInt32LE(Buffer.byteLength(MAGIC_STRING));
-        return {
-            length: length,
-            type: messageBuff.readInt32LE(10),
-            payload: messageBuff.slice(Buffer.byteLength(MAGIC_STRING) + 8).toString()
-        };
-    }
-}
-
-const ipc: IIPC = new IPC();
-
-const sockPath = path.normalize(
-    childProcess
-        .execSync('i3 --get-socketpath')
-        .toString('utf-8')
-        .trim()
+const socketPath = path.normalize(
+	execSync('i3 --get-socketpath')
+		.toString('utf-8')
+		.trim()
 );
 
-ipc.connect(sockPath)
-    .then((socket: net.Socket) => {
-        console.log('Successfully connected to i3-ipc interface.');
+comm.connect(socketPath)
+	.then((socket: Socket) => {
+		console.log('Successfully connected to i3-ipc interface.');
 
-        socket.on('data', (data: Buffer) => {
-           const message = ipc.unpackMessage(data);
-           console.log('reply:', message);
-        });
+		const execMessage = new I3MessageBuilder()
+			.type(I3MessageType.COMMAND)
+			.payload('exec shutter')
+			.build();
 
-        ipc.send(MessageType.COMMAND, 'exec shutter');
-    }, (error) => {
-        console.log(`Error occurs: ${error}`);
-    });
+		console.log('send:', execMessage.payload);
+
+		comm.send(execMessage)
+			.then((reply: I3Reply) => {
+				console.log('reply:', reply.payload);
+			});
+	}, (error) => {
+		console.log(`Error occurs: ${error}`);
+	});
