756a8b45434c984eec48821fb1e46b42c3bd6f06
reeFridge <reefridgerator@gmail.com>
2017-04-13 02:35:42 +0700

    Declare i3 Message interface extended from IMessage

---
+++ src/i3-message.ts
@@ -0,0 +1,19 @@
+import {IMessage} from './i-message';
+
+export const MAGIC = 'i3-ipc';
+
+export enum I3MessageType {
+	COMMAND,
+	GET_WORKSPACES,
+	SUBSCRIBE,
+	GET_OUTPUTS,
+	GET_TREE,
+	GET_MARKS,
+	GET_BAR_CONFIG,
+	GET_VERSION,
+	GET_BINDING_MODES
+}
+
+export interface I3Message extends IMessage {
+	type: I3MessageType;
+}
