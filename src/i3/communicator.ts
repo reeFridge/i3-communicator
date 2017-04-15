445601879e06b864997be5fd1fd540d53db7f5d3
reeFridge <reefridgerator@gmail.com>
2017-04-15 20:56:55 +0700

    Created new i3-communicator class and implemented command method

---
+++ src/i3/communicator.ts
@@ -0,0 +1,22 @@
+import BaseCommunicator from '../base-communicator';
+import {I3Message, I3MessageType, MAGIC} from './message';
+import {I3Reply} from './reply';
+import {I3Result} from './externs/result';
+import I3MessageBuilder from './message-builder';
+
+export default class I3Communicator extends BaseCommunicator<I3Message> {
+	constructor() {
+		super(MAGIC);
+	}
+
+	command(instructions: string): Promise<I3Result[]> {
+		return this.send(
+			new I3MessageBuilder()
+				.type(I3MessageType.COMMAND)
+				.payload(instructions)
+				.build()
+		).then(
+			(reply: I3Reply) => JSON.parse(reply.payload) as I3Result[]
+		);
+	}
+}
