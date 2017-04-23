59b147f63b4c08f448f4861cbcfb37a6f75d3794
reeFridge <reefridgerator@gmail.com>
2017-04-24 01:31:15 +0700

    Added getWorkspaces method

--- src/i3/communicator.ts
+++ src/i3/communicator.ts
@@ -3,6 +3,7 @@ import {I3Message, I3MessageType, MAGIC} from './message';
 import {I3Reply} from './reply';
 import {I3Result} from './externs/result';
 import I3MessageBuilder from './message-builder';
+import {I3Workspace} from "./externs/workspace";
 
 export default class I3Communicator extends BaseCommunicator<I3Message> {
 	constructor() {
@@ -17,6 +18,16 @@ export default class I3Communicator extends BaseCommunicator<I3Message> {
 				.build()
 		).then(
 			(reply: I3Reply) => JSON.parse(reply.payload) as I3Result[]
+		);
+	}
+
+	getWorkspaces(): Promise<I3Workspace[]> {
+		return this.send(
+			new I3MessageBuilder()
+				.type(I3MessageType.GET_WORKSPACES)
+				.build()
+		).then(
+			(reply: I3Reply) => JSON.parse(reply.payload) as I3Workspace[]
 		);
 	}
 }
